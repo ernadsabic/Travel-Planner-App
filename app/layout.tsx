33a2e47e579f8cedac12b555ca5fc0e54fd41298
ernadsabic <ernadsabic923@gmail.com>
2026-07-21 21:24:02 +0200

    fix image url

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
     siteName: "Travel Planner",
     images: [
       {
-        url: "/og-image.png",
+        url: "/og-img.png",
         width: 1200,
         height: 630,
         alt: "Travel Planner App Preview",
